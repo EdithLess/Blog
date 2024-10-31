55af5cfc18d0e5ec9f27e1ad0dea419cca5073a2
Gregory <gregory20korol20@gmail.com>
2024-10-31 06:47:37 +0200

    commit

--- db.js
+++ db.js
@@ -1,12 +1,42 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL,
+// });
+
+// pool.connect((err) => {
+//   if (err) throw err;
+//   console.log("connected to db succesfully");
+// });
+
+// module.exports = pool;
+
+const { Client } = require("pg");
+
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "bond007008",
+  database: "E-shop",
 });
 
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("connected to db succesfully");
-});
+client.connect();
 
-module.exports = pool;
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    client.query(`SELECT * FROM categories`, (err, res) => {
+      if (!err) {
+        const result = res.rows;
+        resolve(result);
+      } else {
+        console.log(err.message);
+        reject(err);
+      }
+    });
+  });
+}
+
+module.exports = {
+  getCategories,
+};
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./db");
+const { getCategories } = require("./db");
 const PORT = process.env.PORT || 3000;
 // const {homepage}=require('./routes')
 const express = require("express");
@@ -9,6 +10,7 @@ app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
 });
 
-app.get("/", (_req, res) => {
-  res.send("hey");
+app.get("/", async (_req, res) => {
+  const category = await getCategories();
+  res.status(200).json(category);
 });
